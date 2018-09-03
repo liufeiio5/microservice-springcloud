a42fcf9f48792a06bb1a20bd21852b405f86491a
fei <liufeiio5@163.com>
2018-09-03 22:46:32 +0800

    update:09032245 重新调试可行

---
+++ microservice-config-server/src/main/java/com/itheima/springboot/ServerApplication.java
@@ -0,0 +1,11 @@
+package com.itheima.springboot;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+@EnableConfigServer
+@SpringBootApplication
+public class ServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+}
